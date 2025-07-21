025016ad64dd892005f2c997d20863ab71925f46
Will Boyle <willboyle18@gmail.com>
2025-07-20 20:25:27 -0700

    Add error checking for not enough arguments

--- src/main.rs
+++ src/main.rs
@@ -25,6 +25,10 @@ struct Config {
 
 impl Config {
     fn new(args: &[String]) -> Config {
+        if args.len() < 3 {
+            panic!("Not enough arguments");
+        }
+
         let query = args[1].clone(); // Using clone for simplicity
         let file_path = args[2].clone();
 
